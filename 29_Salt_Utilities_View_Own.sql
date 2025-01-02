a6e73c94b4816df77d53df8eac6252556a835ed6
Sean1510 <135575040+Sean1510@users.noreply.github.com>
2025-01-02 17:44:52 -0500

    Create 29_Salt_Utilities_View_Own.sql

---
+++ 29_Salt_Utilities_View_Own.sql
@@ -0,0 +1,3 @@
+CREATE VIEW "Home"."29_Salt_Utilities_View_Own" AS
+SELECT "Utility Type", "Billing From", "Billing To", "Total Amount" AS "❶Total Amount", ("Billing To" - "Billing From") AS "❷Total days", "Date Starting To Split", ("Billing To" - "Date Starting To Split") AS "❸Days", round("Total Amount" / ("Billing To" - "Billing From") * ("Billing To" - "Date Starting To Split") / 3, 2) AS "Covered by Sean(=❶/❷*❸/3)", "Paid Date", "Total Amount" / ("Billing To" - "Billing From") / 3 As "Amount Per Day", RANK() OVER (PARTITION BY "Utility Type" ORDER BY "Total Amount" / ("Billing To" - "Billing From") / 3 ASC) AS "Amount Per Day Rank"
+FROM "Home"."29_Salt_Utilities"
