fd7481b079f46e7c2b9f45b2d1d55fbf951a3490
Sean1510 <135575040+Sean1510@users.noreply.github.com>
2025-01-03 09:48:32 -0500

    Update 29_Salt_Utilities_View.sql

--- 29_Salt_Utilities_View.sql
+++ 29_Salt_Utilities_View.sql
@@ -1,11 +1,25 @@
 CREATE VIEW "Home"."29_Salt_Utilities_View" AS
-SELECT 
-    "Utility Type", "Billing From", 
-    "Billing To", 
-    "Total Amount" AS "a - Total Amount", ("Billing To" - "Billing From") AS "b - Total days", 
-    "Date Starting To Split",
-    "Date Ending To Split", 
-    ("Date Ending To Split" - "Date Starting To Split") AS "c - Days Covered by Sean", 
-    round("Total Amount" / ("Billing To" - "Billing From") * ("Date Ending To Split" - "Date Starting To Split") / 3, 2) AS "Covered by Sean(=a/b*c/3)", 
-    "Paid Date"
-FROM "Home"."29_Salt_Utilities"
+SELECT
+  "Utility Type",
+  "Billing From",
+  "Billing To",
+  "Total Amount" AS "a - Total Amount",
+  ("Billing To" - "Billing From") AS "b - Total days",
+  "Date Starting To Split",
+  "Date Ending To Split",
+  ("Date Ending To Split" - "Date Starting To Split") AS "c - Days Covered by Sean",
+  round(
+    (
+      (
+        (
+          "Total Amount" / (("Billing To" - "Billing From"))::numeric
+        ) * (
+          ("Date Ending To Split" - "Date Starting To Split")
+        )::numeric
+      ) / (3)::numeric
+    ),
+    2
+  ) AS "Covered by Sean(=a/b*c/3)",
+  "Paid Date"
+FROM
+  "Home"."29_Salt_Utilities";
