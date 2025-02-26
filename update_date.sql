ea0976ff93ed10c5608e3a661fbef4159cb140fd
Sean1510 <135575040+Sean1510@users.noreply.github.com>
2025-02-26 00:06:19 -0500

    Update update_date.sql

--- update_date.sql
+++ update_date.sql
@@ -1,8 +1,10 @@
 UPDATE "Home"."29_Salt_Utilities"
 SET 
     "Date Ending To Split" = CASE
-        WHEN "Same With Billing To?" = 1 THEN "Billing To"
+        WHEN "Same With Billing To?" = 1 AND "Date Ending To Split" IS NULL THEN "Billing To"
+        ELSE "Date Ending To Split" -- Keep existing value if not null
     END,
     "Date Starting To Split" = CASE
-        WHEN "Same With Billing From?" = 1 THEN "Billing From"
+        WHEN "Same With Billing From?" = 1 AND "Date Starting To Split" IS NULL THEN "Billing From"
+        ELSE "Date Starting To Split" -- Keep existing value if not null
     END
